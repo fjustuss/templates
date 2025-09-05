dc07dc8408add8bc48fd4a033c6f9757614ff7d1
fjustuss <fjustus.art@gmail.com>
2025-09-04 22:00:22 -0300

    Add files via upload

---
+++ nca-toolkit/index.ts
@@ -0,0 +1,33 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: ["WEBHOOK_URL=https://$(EASYPANEL_DOMAIN)"].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 5678,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/home/node/.n8n",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
